eba3cad230c69da49c999a5c736857ace3eeb2a0
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-02 01:24:27 +0100

    feat: setup a basic Express server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,22 @@
-console.log("🤘");
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(
+  cors({
+    origin: `http://localhost:${
+      process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 5173
+    }`,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ it: "works" });
+});
+
+const port = process.env.API_PORT ? parseInt(process.env.API_PORT) : 3000;
+
+app.listen(port, () => {
+  console.log(`🤘 Express server is running at http://localhost:${port}`);
+});
