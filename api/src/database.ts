564a25b3193082cd78393bbe74662a8c48a83aba
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-02 22:41:17 +0100

    feat: Automatically seed fake users at app start if there are none

--- api/src/database.ts
+++ api/src/database.ts
@@ -6,6 +6,8 @@ import {
   MONGO_ROOT_PASSWORD,
   MONGO_ROOT_USERNAME,
 } from "./config";
+import usersService from "./services/usersService";
+import DbCollections from "./types/DbCollections";
 
 let db: Db | null = null;
 
@@ -19,6 +21,19 @@ export const setupDatabase = async () => {
   db = mongoClient.db("core");
 
   console.log("💿 API successfully established MongoDb connection");
+
+  // Seeding fake users for plug and play's sake if there are none
+  try {
+    const existingUsersCount = await db
+      .collection(DbCollections.USERS)
+      .countDocuments({});
+    if (existingUsersCount === 0) {
+      await usersService.seedFakeUsers();
+      console.log("🧌  Fake users seeded successfully");
+    }
+  } catch {
+    console.error("Failed to seed fake users");
+  }
 };
 
 export const getDb = () => {
