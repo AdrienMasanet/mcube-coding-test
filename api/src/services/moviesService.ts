6ddc2bbbf97ef591f74e23f0d9cb20476c9d55d4
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-02 19:39:07 +0100

    feat: enhance getMoviesBySearch and add getMovieById service function

--- api/src/services/moviesService.ts
+++ api/src/services/moviesService.ts
@@ -1,10 +1,20 @@
 import { TMDB_API_KEY, TMDB_BASE_URL } from "../config";
+import { TMDBMovieBase, TMDBMovieDetails } from "../types/TMDBApi";
 
 const moviesServices = {
-  getMoviesBySearch: async (searchString: string): Promise<JSON> => {
+  getMoviesBySearch: async (searchString: string): Promise<TMDBMovieBase[]> => {
     const response = await fetch(
       `${TMDB_BASE_URL}search/movie?query=${searchString}&api_key=${TMDB_API_KEY}`,
     );
+    if (response.status !== 200) throw new Error("TMDB request failed");
+    const result: TMDBMovieBase[] = await response.json();
+    return result;
+  },
+  getMovieById: async (tmdbMovieId: number): Promise<TMDBMovieDetails> => {
+    const response = await fetch(
+      `${TMDB_BASE_URL}movie/${tmdbMovieId}?api_key=${TMDB_API_KEY}`,
+    );
+    if (response.status !== 200) throw new Error("TMDB request failed");
     const result = await response.json();
     return result;
   },
