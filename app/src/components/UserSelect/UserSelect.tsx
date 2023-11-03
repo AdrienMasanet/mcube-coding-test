e17ec2ff979c9294e495437e96d54b5f465267ca
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 14:02:57 +0100

    feat: add the ability to log in by selecting a user

--- app/src/components/UserSelect/UserSelect.tsx
+++ app/src/components/UserSelect/UserSelect.tsx
@@ -1,14 +1,27 @@
+import { ChangeEvent } from "react";
+
+import useAuthentication from "../../hooks/useAuthentication";
 import useUserList from "../../hooks/useUserList";
 import User from "../../types/User";
 import styles from "./UserSelect.module.css";
 
 const UserSelect = () => {
   const { userList } = useUserList();
+  const { loggedInUser, login } = useAuthentication();
+
+  const handleUserSelect = (event: ChangeEvent<HTMLSelectElement>): void => {
+    login(event.target.value);
+  };
 
   return (
     <div className={styles.container}>
-      <select className={styles.select}>
-        <option value="" disabled selected hidden>
+      <select
+        className={styles.select}
+        onChange={handleUserSelect}
+        defaultValue="none"
+        value={loggedInUser ? loggedInUser._id : undefined}
+      >
+        <option value="none" disabled hidden>
           Not logged in yet. Pick a user !
         </option>
         {userList.map((user: User) => (
