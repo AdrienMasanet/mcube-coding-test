1ad4406b19fa04ccef5d50924e438193ce5f29de
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 18:05:20 +0100

    refactor: clean and rename types for frontend

--- app/src/types/Movie.ts
+++ app/src/types/Movie.ts
@@ -1,9 +1,12 @@
 export interface MovieBase {
   title: string;
-  overview: string;
   posterPath: string;
   releaseDate?: Date;
   tmdbMovieId: number;
+}
+
+export interface MovieDetailed extends MovieBase {
+  overview: string;
 }
 
 export interface LibraryMovie extends MovieBase {
