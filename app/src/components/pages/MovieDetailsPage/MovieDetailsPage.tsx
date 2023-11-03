bb384af9316cc5cc076ff47db865de0b9ae5303d
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 18:13:11 +0100

    feat: handle the case where getMovieDetails fails

--- app/src/components/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ app/src/components/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -8,7 +8,7 @@ import styles from "./MovieDetailsPage.module.css";
 const MovieDetailsPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [movie, setMovie] = useState<MovieBase>();
+  const [movie, setMovie] = useState<MovieBase | null>(null);
 
   useEffect(() => {
     if (!location.state || !location.state["tmdbMovieId"]) {
@@ -17,18 +17,24 @@ const MovieDetailsPage = () => {
     }
 
     const fetchMovie = async () => {
-      const movieDetails = await moviesService.getMovieDetails(
-        location.state["tmdbMovieId"],
-      );
-      setMovie(movieDetails);
+      try {
+        const movieDetails = await moviesService.getMovieDetails(
+          location.state["tmdbMovieId"],
+        );
+        setMovie(movieDetails);
+      } catch {
+        setMovie(null);
+      }
     };
 
     fetchMovie();
   }, [location.state, navigate]);
 
+  if (!movie) return;
+
   return (
     <div className={styles.container}>
-      <p>{movie?.title}</p>
+      <p>{movie.title}</p>
     </div>
   );
 };
