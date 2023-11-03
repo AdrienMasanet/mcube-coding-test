4e118be7fc18c60d6eef9a1ba05b122827a4e812
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 16:19:49 +0100

    feat: fetch movies from api when using SearchBar

--- app/src/components/pages/MovieSearchPage/MovieDetailsPage.tsx
+++ app/src/components/pages/MovieSearchPage/MovieDetailsPage.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
+import moviesService from "../../../services/moviesService";
+import { MovieBase } from "../../../types/Movie";
 import styles from "./MovieSearchPage.module.css";
 import SearchBar from "./SearchBar/SearchBar";
 
 const MovieSearchPage = () => {
+  const [movies, setMovies] = useState<MovieBase[]>();
+
+  const fetchMovies = async (searchString: string) => {
+    const fetchedMovies: MovieBase[] =
+      await moviesService.searchMovies(searchString);
+    setMovies(fetchedMovies);
+  };
+
   return (
     <div className={styles.container}>
-      <SearchBar />
+      <SearchBar searchForMovies={fetchMovies} />
     </div>
   );
 };
