2f9ee859d6f3f574477179c3debe2da89eaa4ead
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 17:23:17 +0100

    feat: only show /my-library link if user is authenticated

--- app/src/components/BottomBar/BottomBar.tsx
+++ app/src/components/BottomBar/BottomBar.tsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
 
+import useAuthentication from "../../hooks/useAuthentication";
 import routes from "../../routes";
 import Route from "../../types/Route";
 import styles from "./BottomBar.module.css";
 
 const BottomBar = () => {
+  const { loggedInUser } = useAuthentication();
+
   return (
     <div className={styles.container}>
       <nav className={styles.navlinks}>
         {routes.map((route: Route) => {
+          if (!loggedInUser && route.path === "/my-library") return;
           if (route.linkText)
             return (
               <NavLink
