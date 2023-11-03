463e89f36a706d7ea3d785b9962425530d1d28a1
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 15:16:42 +0100

    feat: enhance BottomBar

--- app/src/components/BottomBar/BottomBar.tsx
+++ app/src/components/BottomBar/BottomBar.tsx
@@ -1,14 +1,22 @@
 import { NavLink } from "react-router-dom";
 
 import routes from "../../routes";
+import Route from "../../types/Route";
 import styles from "./BottomBar.module.css";
 
 const BottomBar = () => {
   return (
     <div className={styles.container}>
-      {routes.map((route) => (
-        <NavLink to={route.path}>Home</NavLink>
-      ))}
+      <nav className={styles.navlinks}>
+        {routes.map((route: Route) => {
+          if (route.linkText)
+            return (
+              <NavLink to={route.path} className={styles.navlink}>
+                {route.linkText}
+              </NavLink>
+            );
+        })}
+      </nav>
     </div>
   );
 };
