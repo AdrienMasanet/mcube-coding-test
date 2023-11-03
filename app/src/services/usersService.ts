0d75210a64f65ed85edc8cef506623fcc32f91a4
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 13:56:46 +0100

    feat: add getAuthenticatedUser service function

--- app/src/services/usersService.ts
+++ app/src/services/usersService.ts
@@ -8,8 +8,33 @@ const usersService = {
         `${API_PROTOCOL}://${API_DOMAIN}:${API_PORT}/users`,
         { method: "GET" },
       );
+
       if (response.status !== 200) throw new Error("API request failed");
       const result: User[] = await response.json();
+      return result;
+    } catch {
+      throw new Error("API request failed");
+    }
+  },
+
+  getAuthenticatedUser: async (): Promise<User | null> => {
+    try {
+      const currentUserId = localStorage.getItem("currentUserId");
+      if (!currentUserId) return null;
+
+      const response = await fetch(
+        `${API_PROTOCOL}://${API_DOMAIN}:${API_PORT}/me`,
+        {
+          method: "GET",
+          headers: {
+            userid: currentUserId,
+          },
+        },
+      );
+
+      if (response.status === 401) return null;
+      if (response.status !== 200) throw new Error("API request failed");
+      const result: User = await response.json();
       return result;
     } catch {
       throw new Error("API request failed");
