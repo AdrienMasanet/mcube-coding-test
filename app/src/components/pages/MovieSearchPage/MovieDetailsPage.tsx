32505fd5a9491113efa199aebff7c1948b21350d
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 17:24:29 +0100

    feat: display MovieGrid on MovieSearchPage

--- app/src/components/pages/MovieSearchPage/MovieDetailsPage.tsx
+++ app/src/components/pages/MovieSearchPage/MovieDetailsPage.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
+import useAuthentication from "../../../hooks/useAuthentication";
 import moviesService from "../../../services/moviesService";
 import { MovieBase } from "../../../types/Movie";
+import MovieGrid from "../../common/MovieGrid/MovieGrid";
 import styles from "./MovieSearchPage.module.css";
 import SearchBar from "./SearchBar/SearchBar";
 
 const MovieSearchPage = () => {
-  const [movies, setMovies] = useState<MovieBase[]>();
+  const [movies, setMovies] = useState<MovieBase[]>([]);
+  const { loggedInUser } = useAuthentication();
 
   const fetchMovies = async (searchString: string) => {
     const fetchedMovies: MovieBase[] =
@@ -17,6 +20,18 @@ const MovieSearchPage = () => {
   return (
     <div className={styles.container}>
       <SearchBar searchForMovies={fetchMovies} />
+      {movies.length > 0 ? (
+        <MovieGrid movies={movies} />
+      ) : (
+        <div className={styles.nosearchcontainer}>
+          <p className={styles.nosearchtext1}>
+            Hello {loggedInUser && loggedInUser?.name}!
+          </p>
+          <p className={styles.nosearchtext2}>
+            Start searching for movies using the search bar 🎬🍿
+          </p>
+        </div>
+      )}
     </div>
   );
 };
