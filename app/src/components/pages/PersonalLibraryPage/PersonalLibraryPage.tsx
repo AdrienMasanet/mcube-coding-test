1f84c4ca9c1808a0703d024c541137e90da4e3e1
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 21:11:55 +0100

    feat: add SortFilter component

--- app/src/components/pages/PersonalLibraryPage/PersonalLibraryPage.tsx
+++ app/src/components/pages/PersonalLibraryPage/PersonalLibraryPage.tsx
@@ -1,12 +1,14 @@
 import useMovieLibrary from "../../../hooks/useMovieLibrary";
 import MovieGrid from "../../common/MovieGrid/MovieGrid";
 import styles from "./PersonalLibraryPage.module.css";
+import SortFilter from "./SortFilter/SortFilter";
 
 const PersonalLibraryPage = () => {
   const { movieLibrary } = useMovieLibrary();
 
   return (
     <div className={styles.container}>
+      <SortFilter />
       <MovieGrid movies={movieLibrary} />
     </div>
   );
