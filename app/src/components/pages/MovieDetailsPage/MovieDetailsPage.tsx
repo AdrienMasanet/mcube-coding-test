1f2382c55468116b2599fea9767c854cdb3fc594
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 18:06:32 +0100

    feat: movie details fetching in MovieDetailsPage

--- app/src/components/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ app/src/components/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import moviesService from "../../../services/moviesService";
+import { MovieBase } from "../../../types/Movie";
 import styles from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState<MovieBase>();
+
+  useEffect(() => {
+    if (!location.state || !location.state["tmdbMovieId"]) {
+      navigate("/");
+      return;
+    }
+
+    const fetchMovie = async () => {
+      const movieDetails = await moviesService.getMovieDetails(
+        location.state["tmdbMovieId"],
+      );
+      setMovie(movieDetails);
+    };
+
+    fetchMovie();
+  }, [location.state, navigate]);
+
   return (
     <div className={styles.container}>
-      <p>MovieDetailsPage</p>
+      <p>{movie?.title}</p>
     </div>
   );
 };
