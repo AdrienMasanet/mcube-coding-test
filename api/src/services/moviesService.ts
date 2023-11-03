0bd62d085c8553c1c039c7f39a00e2616f9eb4ed
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 16:14:00 +0100

    feat: convert fetched data from TMDB to MovieBase[] format

--- api/src/services/moviesService.ts
+++ api/src/services/moviesService.ts
@@ -1,14 +1,33 @@
 import { TMDB_API_KEY, TMDB_BASE_URL } from "../config";
-import { TMDBMovieBase, TMDBMovieDetails } from "../types/TMDBApi";
+import { TMDB_IMAGES_PATH } from "../config";
+import { MovieBase } from "../types/Movie";
+import { TMDBMovieDetails, TMDBResponse } from "../types/TMDBApi";
 
 const moviesServices = {
-  getMoviesBySearch: async (searchString: string): Promise<TMDBMovieBase[]> => {
-    const response = await fetch(
-      `${TMDB_BASE_URL}search/movie?query=${searchString}&api_key=${TMDB_API_KEY}`,
-    );
-    if (response.status !== 200) throw new Error("TMDB request failed");
-    const result: TMDBMovieBase[] = await response.json();
-    return result;
+  getMoviesBySearch: async (searchString: string): Promise<MovieBase[]> => {
+    try {
+      const response = await fetch(
+        `${TMDB_BASE_URL}search/movie?query=${searchString}&api_key=${TMDB_API_KEY}`,
+      );
+      if (response.status !== 200) throw new Error("TMDB request failed");
+      const result: TMDBResponse = await response.json();
+      const parsedMovies: MovieBase[] = result.results.map(
+        (tmdbMovie) =>
+          ({
+            title: tmdbMovie.title,
+            overview: tmdbMovie.overview,
+            posterPath: `${TMDB_IMAGES_PATH}${tmdbMovie.poster_path}`,
+            releaseDate: tmdbMovie.release_date
+              ? new Date(tmdbMovie.release_date)
+              : null,
+            tmdbMovieId: tmdbMovie.id,
+          }) as MovieBase,
+      );
+      return parsedMovies;
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
   },
 
   getMovieById: async (tmdbMovieId: number): Promise<TMDBMovieDetails> => {
