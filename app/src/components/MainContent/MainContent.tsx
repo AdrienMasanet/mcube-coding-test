acb064d1bc5d7a1a5247041a3dbca2ab7c6d493c
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-03 15:05:33 +0100

    feat: show current page in MainContent component

--- app/src/components/MainContent/MainContent.tsx
+++ app/src/components/MainContent/MainContent.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import routes from "../../routes";
 import styles from "./MainContent.module.css";
 
 const MainContent = () => {
   return (
     <div className={styles.container}>
-      <p>MainContent</p>
+      <Routes>
+        {routes.map((route) => (
+          <Route path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </div>
   );
 };
